8a615861b22b2483b71dd3de53ebb3e6b8ab2aed
Yen-Jen-Lo <100771950+Yen-Jen-Lo@users.noreply.github.com>
2022-04-16 23:07:09 +0800

    Add files via upload

--- test0408.v
+++ test0408.v
@@ -2,27 +2,16 @@ module voter_if (I, O);
 input [3:0] I; // I 4 men
 output [3:1] O; // O Result
 reg [3:1] O;
-always@ (I)
-begin
-    if (I == 4'b0000) O[3] = 1;
-    else if (I == 4'b0001) O[3] = 1;
-    else if (I == 4'b0010) O[3] = 1;
-    else if (I == 4'b0100) O[3] = 1;
-    else if (I == 4'b1000) O[3] = 1;
-    else O[3] = 0;
-    if (I == 4'b0011) O[2] = 1;
-    else if (I == 4'b0101) O[2] = 1;
-    else if (I == 4'b0110) O[2] = 1;
-    else if (I == 4'b1001) O[2] = 1;
-    else if (I == 4'b1010) O[2] = 1;
-    else if (I == 4'b1100) O[2] = 1;
-    else O[2] = 0;
 
-    if (I == 4'b1110 || I == 4'b1101 || I ==
-            4'b1011 || I == 4'b0111 || I == 4'b1111)
-        O[1] = 1;
-    else
-        O[1] = 0;
+always @(I)
 
-end
+case (I)
+    4'b0000||4'b0001||4'b0010||4'b0100||4'b1000:
+        O[3:1] = 3'b100;
+    4'b0011||4'b0101||4'b0110||4'b1001||4'b1010||4'b1100:
+        O[3:1] = 3'b010;
+    4'b1110||4'b1101|| 4'b1011 || 4'b0111 ||4'b1111:
+        O[3:1] =3'b001;
+    default: O[3:1] =3'b000;
+endcase
 endmodule
